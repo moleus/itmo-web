95589415f0f4eef0520c7a25c212209c2ce83a9b
Moleus <nomail.com>
2022-09-15 12:23:35 +0300

    Add webpack configs.

---
+++ webpack.common.js
@@ -0,0 +1,48 @@
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+
+module.exports = {
+    entry: "./scripts/main.ts",
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.svg$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+        }),
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+            inject: 'body'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name][contenthash].css",
+        }),
+        new CleanWebpackPlugin(),
+    ],
+};
