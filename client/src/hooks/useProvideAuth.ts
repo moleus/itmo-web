6dd634bbdfb24d90984093742ca1fa0d5f68801f
Moleus <fafufuburr@gmail.com>
2022-12-03 15:25:41 +0300

    delete: remove unused auth components.

--- client/src/hooks/useProvideAuth.ts
+++
@@ -1,6 +0,0 @@
-import {useContext} from "react";
-import {AuthContext} from "../components/auth/AuthProvider";
-
-export default function useProvideAuth() {
-     return useContext(AuthContext);
-}
